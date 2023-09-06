dba03e4c0e8ed98fe5a05c18077255b3fbcad1dc
Zakaria Noussi <quipster.zak@gmail.com>
2023-09-06 11:32:43 +0000

    added comments, changed username

--- Intro/hello_test.go
+++ Intro/hello_test.go
@@ -4,11 +4,11 @@ import (
 	"testing"
 )
 
-func TestHello(t *testing.T) {
+func TestHello(t *testing.T) { // t *testing.T being a function signature, t being a *testing.T pointer, which report test failure.
 	got := Hello()
 	want := "Hello, world"
 
 	if got != want {
-		t.Errorf("got %q want %q", got, want)
+		t.Errorf("got %q want %q", got, want) //t.Errorf report test failure by formatting the error message and marking the test as failed.
 	}
 }
