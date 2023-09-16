6c82a9c2c95ed736a81513b73b8aa6bed9a03ce7
Zakaria Noussi <quipster.zak@gmail.com>
2023-09-16 16:24:12 +0000

    added counter, example function, aswell as used strings package

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,9 +1,13 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
+	repeated := Repeat("a", 5)
 	expected := "aaaaa"
 
 	if repeated != expected {
@@ -13,6 +17,24 @@ func TestRepeat(t *testing.T) {
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 5)
+	}
+}
+
+// ExampleRepeat demonstrates how to use the Repeat function.
+func ExampleRepeat() {
+	result := Repeat("a", 5)
+	fmt.Println(result)
+	// Output: aaaaa
+}
+
+func TestToUpper(t *testing.T) {
+	input := "zak"
+	expected := "ZAK"
+
+	result := strings.ToUpper(input)
+
+	if result != expected {
+		t.Errorf("expected %q got %q", expected, result)
 	}
 }
