2161dbbfaf29f12542ee0f3f5d2ad31854a467fa
Zakaria Noussi <quipster.zak@gmail.com>
2023-09-16 17:16:51 +0000

    using make(t type, size ...int)

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -1,9 +1,24 @@
 package main
 
-func Sum(numbers [5]int) int {
+func SumArray(numbers [5]int) int {
 	sum := 0
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
+}
+func SumSlice(numbers []int) int {
+	sum := 0
+	for _, number := range numbers {
+		sum += number
+	}
+	return sum
+}
+func SumAll(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+	for i, numbers := range numbersToSum {
+		sums[i] = SumSlice(numbers)
+	}
+	return sums
 }
